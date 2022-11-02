85a1f9126cebd2b6d2fb3304198271ebba4d8ebc
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-11-02 20:55:42 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         double f,c;         c=sc.nextDouble();         f=32+(c*9/5);         System.out.printf("%.2f",f);     } }

---
+++ Temperature_Conversion.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double f,c;
+        c=sc.nextDouble();
+        f=32+(c*9/5);
+        System.out.printf("%.2f",f);
+    }
+}
