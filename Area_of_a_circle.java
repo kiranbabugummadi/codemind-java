addd4a361ff6f2b6ae2bd14d21a0ca140b1a6c30
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-11-02 20:58:39 +0530

    import java.util.*; public class file {     public static void main(String[] args)     {         Scanner sc = new Scanner(System.in);         double r,area;         r=sc.nextDouble();         area=3.14*r*r;         System.out.printf("%.2f",area);     } }

---
+++ Area_of_a_circle.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        double r,area;
+        r=sc.nextDouble();
+        area=3.14*r*r;
+        System.out.printf("%.2f",area);
+    }
+}
