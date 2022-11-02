4af12aaaad9dacf5fe6efb778110d66ac6f44846
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-11-02 21:18:41 +0530

    import java.util.*; public class file {     public static void main(String[] args) {     Scanner sc = new Scanner(System.in);     double a,b,av;     a=sc.nextDouble();     b=sc.nextDouble();     av=(a+b)/2;     System.out.printf("%.4f",av); } }

---
+++ Find_the_average_of_two_numbers.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class file
+{
+    public static void main(String[] args)
+{
+    Scanner sc = new Scanner(System.in);
+    double a,b,av;
+    a=sc.nextDouble();
+    b=sc.nextDouble();
+    av=(a+b)/2;
+    System.out.printf("%.4f",av);
+}
+}
