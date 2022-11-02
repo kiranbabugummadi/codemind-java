c7f2acb548d8078f37a0c586cd41de4c335b529e
kiranbabugummadi <111487733+kiranbabugummadi@users.noreply.github.com>
2022-11-02 21:21:25 +0530

    import java.util.*; public class file {
        public static void main(String[] args) {
            
            Scanner sc = new Scanner(System.in);
            char c = sc.next().charAt(0);
            int as_chi = c;
            System.out.println(as_chi);
        }
    }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class file {
+
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        int as_chi = c;
+        System.out.println(as_chi);
+    }
+}
